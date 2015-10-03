66d51f58bbe348eb52a1bd6a9cf05162fec52728
Pat McGhen <patrickmcghen@gmail.com>
2015-10-02 20:32:20 -0400

    Add fitvids

--- header.php
+++ header.php
@@ -15,6 +15,16 @@
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-68345434-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+
 		<a class="access skip-link" href="#main-content">Skip to content</a>
 
 		<header class="header-main" role="banner">
--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -10,6 +10,10 @@
 		// Main stylesheet
 		wp_register_style('pgg-css', get_template_directory_uri() . '/lib/css/style.css', array(), '20150813', 'all');
 		wp_enqueue_style('pgg-css');
+
+		// JavaScript stuff
+		wp_enqueue_script('fitvids', get_template_directory_uri() . '/lib/js/vendor/fitvids.min.js', array('jquery'), '', true);
+		wp_enqueue_script('pgg-js', get_template_directory_uri() . '/lib/js/pharaohs.js', array('jquery'), '', true);
 	}
 
 	add_action('wp_enqueue_scripts', 'pgg_scripts');
